4bbff75747e74432bed6f05386749b08ae8df705
TachafineAittoudaWebpick <t.aittouda@webpick.info>
2023-01-12 05:00:49 +0100

    Fixe maria db password

--- scripts/install_mariadb.sh
+++ scripts/install_mariadb.sh
@@ -1,6 +1,7 @@
 #install mariadbserver and its dependencies 
 apt-get install mariadb-server mariadb-client -y
 # set the mariadbserver root password
+read -p "Enter the password for the MARIADB root: " MYSQL_ROOT_PASSWORD
 mysqladmin -u root password $MYSQL_ROOT_PASSWORD 
 # set the mariadbserver root password
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';"
--- scripts/set_mariadb_password.sh
+++ scripts/set_mariadb_password.sh
@@ -1,3 +1,9 @@
 # set mariadb password for the user root
-read -p "Enter the password for the user root: " MYSQL_ROOT_PASSWORD 
-export MYSQL_ROOT_PASSWORD 
+read -p "Enter the password for the user root: " MYSQL_ROOT_PASSWORD
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';"
+#writing the password to the file /admin/.dbpass
+echo $MYSQL_ROOT_PASSWORD > /admin/.dbpass
+#set the permissions for the file /admin/.dbpass
+chmod 600 /admin/.dbpass
+#set the owner of the file /admin/.dbpass
+chown root:root /admin/.dbpass
