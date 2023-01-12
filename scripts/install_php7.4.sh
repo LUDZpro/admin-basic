a48586d37e720ed2bcb3ed1ba375add5a304da4e
TachafineAittoudaWebpick <t.aittouda@webpick.info>
2023-01-12 04:42:44 +0100

    Fixe php script order

--- scripts/install_php7.4.sh
+++ scripts/install_php7.4.sh
@@ -5,7 +5,28 @@ apt-get update
 # install the php7.4-fpm package and its dependencies and the php7.4-cli
 apt-get install php7.4-fpm php7.4-cli -y
 # install the php extensions 
-apt-get install php7.4-common php7.4-mysql php7.4-xml php7.4-curl php7.4-json php7.4-gd php7.4-mbstring php7.4-zip php7.4-bcmath php7.4-intl php7.4-soap php7.4-xmlrpc php7.4-ldap php7.4-imagick php7.4-redis php7.4-memcached php7.4-xdebug php7.4-sqlite3 php7.4-pgsql php7.4-bz2 php7.4-gmp php7.4-odbc php7.4-ldap php7.4-xmlrpc php7.4-xml php7.4-zip php7.4-mbstring php7.4-soap php7.4-intl php7.4-bcmath php7.4-gd php7.4-json php7.4-curl php7.4-xml php7.4-mysql php7.4-common -y
+apt-get install php7.4-common \
+php7.4-mysql \
+php7.4-xml \
+php7.4-curl \
+php7.4-json \
+php7.4-gd \
+php7.4-mbstring \
+php7.4-zip \
+php7.4-bcmath \
+php7.4-intl \
+php7.4-soap \
+php7.4-xmlrpc \
+php7.4-ldap \
+php7.4-imagick \
+php7.4-redis \
+php7.4-memcached \
+php7.4-xdebug \
+php7.4-sqlite3 \
+php7.4-pgsql \
+php7.4-bz2 \
+php7.4-gmp \
+php7.4-odbc -y
 # enable the php7.4-fpm service and its extensions
 systemctl enable php7.4-fpm
 systemctl start php7.4-fpm
