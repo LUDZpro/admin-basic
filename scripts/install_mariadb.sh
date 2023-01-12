546c767577baf1f6497a76daddcc07cb72a01eba
TachafineAittoudaWebpick <t.aittouda@webpick.info>
2023-01-12 05:06:11 +0100

    Fixe maria db password warning

--- scripts/install_mariadb.sh
+++ scripts/install_mariadb.sh
@@ -3,5 +3,3 @@ apt-get install mariadb-server mariadb-client -y
 # set the mariadbserver root password
 read -p "Enter the password for the MARIADB root: " MYSQL_ROOT_PASSWORD
 mysqladmin -u root password $MYSQL_ROOT_PASSWORD 
-# set the mariadbserver root password
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';"
