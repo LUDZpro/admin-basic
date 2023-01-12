6d486fefeeb8111c8707f73f1678f402056e7b08
TachafineAittoudaWebpick <t.aittouda@webpick.info>
2023-01-12 05:04:40 +0100

    Fixe maria db password reset

--- scripts/set_mariadb_password.sh
+++ scripts/set_mariadb_password.sh
@@ -1,6 +1,6 @@
 # set mariadb password for the user root
-read -p "Enter the password for the user root: " MYSQL_ROOT_PASSWORD
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';"
+read -p "Enter the password for the MARIADB root: " MYSQL_ROOT_PASSWORD
+mysqladmin -u root password $MYSQL_ROOT_PASSWORD 
 #writing the password to the file /admin/.dbpass
 echo $MYSQL_ROOT_PASSWORD > /admin/.dbpass
 #set the permissions for the file /admin/.dbpass
